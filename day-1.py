75c33b954f627d4191ed434b9f1b6d00c7c0a073
firdausmhd <firdaus9224@gmail.com>
2025-12-04 11:48:49 +0800

    day-1
    Day 1 — Python Basic: Print and Variables

---
+++ day-1.py
@@ -0,0 +1,22 @@
+#exercise-day-1
+name = "Firdaus"
+age = 33
+fav_language = "MySQL and Python"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My favorite language is" ,fav_language)
+
+#mini-project
+name = "Firdaus"
+age = 33
+fav_language = "MySQL and Python"
+
+print("=====================")
+print("   PERSONAL INFO     ")
+print("=====================")
+print("Name:",name)
+print("Age:",age)
+print("Language:",fav_language)
+
+
